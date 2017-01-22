16325ad937f54af62883a88c207190f6ddcb0e80
docodon <dhsharma27@gmail.com>
2017-01-22 12:11:16 +0530

    mobile client testing on production

--- app/controllers/api/v0/base_controller.rb
+++ app/controllers/api/v0/base_controller.rb
@@ -7,9 +7,14 @@ module Api
 
     	def set_current_user
  			begin
+ 				p 'HEADERS from mobile !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+ 				p request.headers
  				user = TokenDecrypter.decode_params(request.headers["event-token"])			
  				@current_user = User.find_by_flock_id(user["userId"])
+ 				p @current_user
  			rescue Exception => e
+ 				p 'ERROR MESSAGE FOR MOBILE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11111'
+ 				p e.message
  			   	render json: {status: false}, status: 422
  			end	
     	end
--- lib/token_decrypter.rb
+++ lib/token_decrypter.rb
@@ -3,6 +3,7 @@ require 'jwt'
 class TokenDecrypter
  
   def self.decode_params(token)
+  	p 'TOKEN MOBILE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'+token
     JWT.decode(token, ENV['APP_SECRET'], true, { :algorithm => 'HS256' })[0]    
   end
 
