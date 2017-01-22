f1b82ec646b02791f811c02acc237488b7644711
docodon <dhsharma27@gmail.com>
2017-01-22 11:44:40 +0530

    zomato key

--- public/scripts/config.e7fd92ac.js
+++ public/scripts/config.e7fd92ac.js
@@ -2,6 +2,6 @@
 
  angular.module('config', [])
 
-.constant('ENV', {name:'production',apiEndpoint:'https://murmuring-brushlands-77259.herokuapp.com',zomato_api_url:'https://developers.zomato.com/api/v2.1',pagination_size:20,zomato_key:''})
+.constant('ENV', {name:'production',apiEndpoint:'https://murmuring-brushlands-77259.herokuapp.com',zomato_api_url:'https://developers.zomato.com/api/v2.1',pagination_size:20,zomato_key:ENV['ZOMATO_KEY']})
 
 ;
