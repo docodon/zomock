33606e026f9940a7b2a5025765d494e6702f5840
docodon <dhsharma27@gmail.com>
2017-01-22 10:45:34 +0530

    angular build commited again after cleaning existing commits

---
+++ public/scripts/config.e7fd92ac.js
@@ -0,0 +1,7 @@
+"use strict";
+
+ angular.module('config', [])
+
+.constant('ENV', {name:'production',apiEndpoint:'https://murmuring-brushlands-77259.herokuapp.com',zomato_api_url:'https://developers.zomato.com/api/v2.1',pagination_size:20,zomato_key:''})
+
+;
