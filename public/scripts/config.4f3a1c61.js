a395a0aa0ceac37b327ae1922f819715e32251e2
docodon <dhsharma27@gmail.com>
2017-02-19 08:59:18 +0530

    fix

--- public/scripts/config.4f3a1c61.js
+++ public/scripts/config.4f3a1c61.js
@@ -2,6 +2,6 @@
 
  angular.module('config', [])
 
-.constant('ENV', {name:'production',apiEndpoint:'https://c9a8cdbe.ngrok.io',zomato_api_url:'https://developers.zomato.com/api/v2.1',pagination_size:20,flock_api_url:'https://api.flock.co',zomato_key:ENV['ZOMATO_KEY']})
+.constant('ENV', {name:'production',apiEndpoint:'https://murmuring-brushlands-77259.herokuapp.com',zomato_api_url:'https://developers.zomato.com/api/v2.1',pagination_size:20,flock_api_url:'https://api.flock.co',zomato_key:ENV['ZOMATO_KEY']})
 
 ;
